37e7351cb84600191df5d9244e6dc165ca4d5984
Abhishek Gupta <abhishekkrgupta117@gmail.com>
2022-09-17 01:34:50 -0700

    paragraph is added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-
+import Header from './Common/header.js';
 function App() {
   return (
     <div className="App">
       <h1>Local Resto</h1>
+      <p>Website is under maintence</p>
+      <Header/>
     </div>
   );
 }
